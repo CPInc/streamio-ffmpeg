759b81fe3064f9cf2c2ffa2be4d76c46656efade
Emmanuel <emmanuel@tachyonlabs.dev>
2025-02-19 10:41:09 +0300

    rotatino -90

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -152,11 +152,11 @@ module FFMPEG
     end
 
     def width
-      rotation.nil? || rotation == 180 ? @width : @height;
+      rotation.nil? || rotation == -90 || rotation == 180 ? @width : @height
     end
 
     def height
-      rotation.nil? || rotation == 180 ? @height : @width;
+      rotation.nil? || rotation == -90 || rotation == 180 ? @height : @width
     end
 
     def resolution
